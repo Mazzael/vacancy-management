b04ab3e7475a2569a514144334bd9bee259be581
vmazza <vtrujillomazza@gmail.com>
2024-08-17 09:01:36 -0300

    feat: jwt configuration

--- src/main/java/br/com/vmazza/vacancy_management/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/vmazza/vacancy_management/modules/company/controllers/AuthCompanyController.java
@@ -3,6 +3,8 @@ package br.com.vmazza.vacancy_management.modules.company.controllers;
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,16 @@ public class AuthCompanyController {
     private AuthCompanyUseCase authCompanyUseCase;
 
     @PostMapping("/company")
-    public String create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
 
-        return this.authCompanyUseCase.execute(authCompanyDTO);
+        try {
+            var result = this.authCompanyUseCase.execute(authCompanyDTO);
+
+            return ResponseEntity.ok().body(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
 
     }
     
--- src/main/java/br/com/vmazza/vacancy_management/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/vmazza/vacancy_management/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.vmazza.vacancy_management.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +42,7 @@ public class AuthCompanyUseCase {
         }
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        var token = JWT.create().withIssuer("javagas").withSubject(company.getId().toString()).sign(algorithm);
+        var token = JWT.create().withIssuer("javagas").withExpiresAt(Instant.now().plus(Duration.ofHours(2))).withSubject(company.getId().toString()).sign(algorithm);
 
         return token;
     }
