2782043e6e502c47b050f024863b3ea5a502d8fb
vmazza <vtrujillomazza@gmail.com>
2024-08-17 18:49:57 -0300

    feat: add roles and expires in to company token

---
+++ src/main/java/br/com/vmazza/vacancy_management/modules/company/dto/AuthCompanyResponseDTO.java
@@ -0,0 +1,17 @@
+package br.com.vmazza.vacancy_management.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCompanyResponseDTO {
+
+    private String accessToken;
+    private Long expiresIn;
+    
+}
