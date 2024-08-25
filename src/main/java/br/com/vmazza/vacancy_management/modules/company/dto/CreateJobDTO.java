37461cb9aa5d0d9701ff46c2a62f707fb12bb1c0
vmazza <vtrujillomazza@gmail.com>
2024-08-25 13:05:29 -0300

    docs: refactor create job schemas

--- src/main/java/br/com/vmazza/vacancy_management/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/vmazza/vacancy_management/modules/company/dto/CreateJobDTO.java
@@ -1,12 +1,19 @@
 package br.com.vmazza.vacancy_management.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
+    @Schema(example = "Vacancy for junior developer", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Total pass, stock options", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "JUNIOR", requiredMode = RequiredMode.REQUIRED)
     private String level;
     
 }
--- src/main/java/br/com/vmazza/vacancy_management/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/vmazza/vacancy_management/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -28,10 +29,15 @@ public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Schema(example = "Vacancy for designer")
     private String description;
+
+    @Schema(example = "Stock options")
     private String benefits;
 
     @NotBlank(message = "This field is required")
+    @Schema(example = "MID LEVEL")
     private String level;
 
     @ManyToOne()
