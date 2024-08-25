951a56ebd3fad2ec2a64107a69d6d1d099738ba9
vmazza <vtrujillomazza@gmail.com>
2024-08-25 13:23:41 -0300

    docs: refactor swagger config

--- src/main/java/br/com/vmazza/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/br/com/vmazza/vacancy_management/VacancyManagementApplication.java
@@ -3,21 +3,7 @@ package br.com.vmazza.vacancy_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-
 @SpringBootApplication
-@OpenAPIDefinition(
-	info = @Info(
-		title = "Vacancy Management",
-		description = "API resposible for vacancy management",
-		version = "1"
-	)
-)
-@SecurityScheme(name = "jwt_auth", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class VacancyManagementApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/vmazza/vacancy_management/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package br.com.vmazza.vacancy_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+        .info(new Info().title("Vacancy Management")
+        .description("API resposible for vacancy management")
+        .version("1"))
+        .schemaRequirement("jwt_auth", createSecurityScheme());
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+        .name("jwt_auth")
+        .type(SecurityScheme.Type.HTTP)
+        .scheme("bearer")
+        .bearerFormat("JWT");
+    }
+    
+}
